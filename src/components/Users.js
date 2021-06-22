80f91286233da2724e941930ba4b348830a0def7
Anirup Patnaik <anirup049@gmail.com>
2021-06-22 23:55:09 +0530

    Table UI and binding along with state management

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,27 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import classes from "./Users.module.css";
 
-const _handleFetchCall = () => {
-  const users = ["aniruppat", "reddy-s"];
-
-  users.forEach(async (user) => {
-    try {
-      let request = await fetch(`https://api.github.com/users/${user}`);
-      console.log(request);
-      if (request.status !== 200) {
-        throw new Error("Something went wrong.");
-      }
-      let resposne = await request.json();
-      console.log(resposne);
-    } catch (error) {
-      alert(error.message);
-    }
-  });
-};
 const Users = () => {
+  const [usersData, setUsersData] = useState([]);
   useEffect(() => {
     _handleFetchCall();
   }, []);
-  return <div></div>;
+
+  const _handleFetchCall = () => {
+    const users = ["aniruppat", "reddy-s"];
+    users.forEach(async (user) => {
+      try {
+        let request = await fetch(`https://api.github.com/users/${user}`);
+        if (request.status !== 200) {
+          throw new Error("Something went wrong.");
+        }
+        let resposne = await request.json();
+        setUsersData((prevData) => {
+          return [...prevData, resposne];
+        });
+      } catch (error) {
+        alert(error.message);
+      }
+    });
+  };
+
+  console.log(usersData);
+
+  return (
+    <div>
+      <table className={classes.table}>
+        <tr>
+          <th>Name</th>
+          <th>Bio</th>
+        </tr>
+        {usersData.map((user) => (
+          <tr>
+            <td>{user.name}</td>
+            <td>{user.bio}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 };
 
 export default Users;
